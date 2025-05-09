eff28d246b6b52b7bd94dfda0082cf9273afd549
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-05-09 09:54:36 +0200

    update

--- src/main/java/de/fred4jupiter/spring/security/oauth/demo/PingController.java
+++ src/main/java/de/fred4jupiter/spring/security/oauth/demo/PingController.java
@@ -1,17 +1,15 @@
 package de.fred4jupiter.spring.security.oauth.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class PingController {
 
     @GetMapping("/ping")
-    public String ping() {
-        return "ping " + LocalDateTime.now();
+    public PingResult ping() {
+        return new PingResult("ping " + LocalDateTime.now());
     }
 }
