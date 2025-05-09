9e0663ab5fb250d41641359a95760b9a303019bf
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-05-09 16:50:31 +0200

    update config

--- spring-security-oauth-client/src/main/java/de/fred4jupiter/spring/security/oauth/client/PingRestClient.java
+++ spring-security-oauth-client/src/main/java/de/fred4jupiter/spring/security/oauth/client/PingRestClient.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -19,7 +20,11 @@ public class PingRestClient {
     }
 
     public String callPingOnServer() {
-        ResponseEntity<String> responseEntity = restClient.get().uri("http://localhost:8280/ping").retrieve().toEntity(String.class);
+        ResponseEntity<String> responseEntity = restClient.get()
+                .uri("http://localhost:8280/ping")
+//                .attributes(RequestAttributeClientRegistrationIdResolver.clientRegistrationId("demo-client"))
+                .retrieve()
+                .toEntity(String.class);
 
         final HttpStatusCode statusCode = responseEntity.getStatusCode();
         if (statusCode.is2xxSuccessful()) {
--- spring-security-oauth-client/src/main/java/de/fred4jupiter/spring/security/oauth/client/config/ClientConfig.java
+++ spring-security-oauth-client/src/main/java/de/fred4jupiter/spring/security/oauth/client/config/ClientConfig.java
@@ -1,18 +1,31 @@
 package de.fred4jupiter.spring.security.oauth.client.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfig {
 
     @Bean
-    public RestClient restClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+    public RestClient restClient(OAuth2AuthorizedClientManager authorizedClientManager,
+                                 OAuth2ClientHttpRequestInterceptor.ClientRegistrationIdResolver clientRegistrationIdResolver) {
         OAuth2ClientHttpRequestInterceptor requestInterceptor = new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
-        return RestClient.builder().requestInterceptor(requestInterceptor).build();
+        requestInterceptor.setClientRegistrationIdResolver(clientRegistrationIdResolver);
+
+        return RestClient.builder()
+                .requestInterceptor(requestInterceptor)
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public OAuth2ClientHttpRequestInterceptor.ClientRegistrationIdResolver clientRegistrationIdResolver() {
+        return new RequestAttributeClientRegistrationIdResolver();
     }
 
 //    /**
