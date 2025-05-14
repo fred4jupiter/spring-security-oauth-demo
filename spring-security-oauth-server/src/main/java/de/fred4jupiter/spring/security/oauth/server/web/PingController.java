59abc1231b29b3d5e4459bfa2f3b75dc1e8c514d
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-05-14 10:52:45 +0200

    update PingController

--- spring-security-oauth-server/src/main/java/de/fred4jupiter/spring/security/oauth/server/web/PingController.java
+++ spring-security-oauth-server/src/main/java/de/fred4jupiter/spring/security/oauth/server/web/PingController.java
@@ -2,10 +2,13 @@ package de.fred4jupiter.spring.security.oauth.server.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @RestController
 public class PingController {
@@ -13,8 +16,12 @@ public class PingController {
     private static final Logger LOG = LoggerFactory.getLogger(PingController.class);
 
     @GetMapping("/ping")
-    public PingResult ping() {
-        LOG.info("ping endpoint was called");
+    public PingResult ping(Authentication authentication) {
+        LOG.info("ping endpoint was called. authentication={}", authentication);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        LOG.info("authorities={}", authorities);
+
         return new PingResult("ping " + LocalDateTime.now());
     }
 }
