2041c54652f363a22d093913562cf59bcd689565
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-06-25 09:41:07 +0200

    added webpapp as an example for authorization code grant flow

---
+++ oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/WebappApplication.java
@@ -0,0 +1,13 @@
+package de.fred4jupiter.spring.security.oauth.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+}
---
+++ oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/web/HomeController.java
@@ -0,0 +1,17 @@
+package de.fred4jupiter.spring.security.oauth.webapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("username", "Freddy");
+        return "home";
+    }
+}
