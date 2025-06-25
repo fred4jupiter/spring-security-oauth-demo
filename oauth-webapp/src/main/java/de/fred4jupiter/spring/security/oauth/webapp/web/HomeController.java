1e5d9bfb978985739d9343c65fe79ec381846f6e
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-06-25 12:16:36 +0200

    update

--- oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/web/HomeController.java
+++ oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/web/HomeController.java
@@ -1,6 +1,7 @@
 package de.fred4jupiter.spring.security.oauth.webapp.web;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping
-    public String home(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
-        model.addAttribute("username", oAuth2AuthenticationToken.getPrincipal().getAttribute("preferred_username"));
-        model.addAttribute("authorities", oAuth2AuthenticationToken.getAuthorities());
-        model.addAttribute("name", oAuth2AuthenticationToken.getPrincipal().getAttribute("name"));
+    public String home(Model model, @AuthenticationPrincipal OidcUser user) {
+        model.addAttribute("username", user.getUserInfo().getClaimAsString("preferred_username"));
+        model.addAttribute("authorities", user.getAuthorities());
+        model.addAttribute("name", user.getUserInfo().getClaimAsString("name"));
         return "home";
     }
 }
