965fb180e6f644e17f936cc2c1cdc95c71409b30
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-06-26 11:27:32 +0200

    showing claims in bootstrap table

--- oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/web/HomeController.java
+++ oauth-webapp/src/main/java/de/fred4jupiter/spring/security/oauth/webapp/web/HomeController.java
@@ -1,5 +1,6 @@
 package de.fred4jupiter.spring.security.oauth.webapp.web;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/home")
@@ -16,7 +18,7 @@ public class HomeController {
     @GetMapping
     public String home(Model model, @AuthenticationPrincipal OidcUser user) {
         model.addAttribute("username", user.getUserInfo().getClaimAsString("preferred_username"));
-        model.addAttribute("authorities", user.getAuthorities());
+        model.addAttribute("authorities", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
         model.addAttribute("name", user.getUserInfo().getClaimAsString("name"));
 
         List<ClaimEntry> claimEntries = user.getClaims().entrySet().stream().map(entry -> new ClaimEntry(entry.getKey(), entry.getValue())).toList();
